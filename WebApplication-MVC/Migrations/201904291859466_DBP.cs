fa64dbc32cd131aa264d2e0c400f549170bc0a66
Navid Sohrabi <navid.sohrabi@gmail.com>
2019-05-01 14:07:24 +0430

    init

---
+++ WebApplication-MVC/Migrations/201904291859466_DBP.cs
@@ -0,0 +1,28 @@
+namespace WebApplication_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DBP : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        BrandName = c.String(nullable: false),
+                        ModelNo = c.String(nullable: false),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
